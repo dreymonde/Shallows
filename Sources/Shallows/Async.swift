6a9048982e433f95f0a9818046e34eb9f8cd2f9b
Oleg Dreyman <odreyman@surprisehr.com>
2022-04-23 19:18:45 +0300

    add special async funcs for `Void` keys

--- Sources/Shallows/Async.swift
+++ Sources/Shallows/Async.swift
@@ -18,10 +18,38 @@ extension ReadableStorageProtocol {
 @available(macOS 10.15.0, *)
 @available(watchOS 6.0, *)
 @available(tvOS 13.0.0, *)
+extension ReadableStorageProtocol where Key == Void {
+    public func retrieve() async throws -> Value {
+        return try await withCheckedThrowingContinuation({ (continuation) in
+            self.retrieve() { result in
+                continuation.resume(with: result)
+            }
+        })
+    }
+}
+
+@available(iOS 13.0.0, *)
+@available(macOS 10.15.0, *)
+@available(watchOS 6.0, *)
+@available(tvOS 13.0.0, *)
 extension WritableStorageProtocol {
     public func set(_ value: Value, forKey key: Key) async throws -> Void {
         return try await withCheckedThrowingContinuation({ (continuation) in
             self.set(value, forKey: key) { result in
+                continuation.resume(with: result)
+            }
+        })
+    }
+}
+
+@available(iOS 13.0.0, *)
+@available(macOS 10.15.0, *)
+@available(watchOS 6.0, *)
+@available(tvOS 13.0.0, *)
+extension WritableStorageProtocol where Key == Void {
+    public func set(_ value: Value) async throws -> Void {
+        return try await withCheckedThrowingContinuation({ (continuation) in
+            self.set(value) { result in
                 continuation.resume(with: result)
             }
         })
